e16d24c2377f2469e2f79524af7b8b1431f9e90c
bnjamel <malfaris4771@gmail.com>
2023-11-17 23:23:34 +0300

    added the upload file feature

--- src/pages/student/Step1.jsx
+++ src/pages/student/Step1.jsx
@@ -1,9 +1,26 @@
 import React, { useState } from "react";
 import { Button } from "@material-tailwind/react";
 import { HiOutlineUpload } from "react-icons/hi";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 function Step1({ handleNext, handlePrev }) {
-  const [file, setfile] = useState();
+  const [file, setFile] = useState();
+  const [fileName, setFileName] = useState("No file selected");
+
+  const types = ['image/png', 'image/jpeg'];
+
+  const handleImageUpload = (e) => {
+    const selected = e.target.files[0]
+    if(selected) {
+      setFileName(selected.name)
+      setFile(URL.createObjectURL(e.target.files[0]))
+    }
+  }
+
+  const removeImage = () => {
+    setFileName("No file selected")
+    setFile(null)
+  }
 
   return (
     <div>
@@ -71,10 +88,22 @@ function Step1({ handleNext, handlePrev }) {
                   <HiOutlineUpload className="text-2xl text-white " />
                 </div>
               </div>
-              <input id="file" name="file" type="file" className="hidden" />
-              {file && <div className="w-20 h-20 bg-black  bg-[url()]"></div>}
+              <input id="file" name="file" type="file" className="hidden" accept={types} onChange={handleImageUpload} />
+              {
+                file && (
+                  <>
+                    <img src={file} className='img w-[50px] h-[50px] object-contain'/>
+                    <p className="text-[18px] font-cairoSemiBold">{fileName}</p>
+                  </>
+                )
+              }
             </label>
           </div>
+          {file && (
+              <button className="my-2 bg-red-400 p-2 rounded hover:opacity-80 transition ease-in-out" onClick={removeImage}>
+                <FaRegTrashAlt className="text-[24px] text-light-100" />
+              </button> 
+            )}
         </form>
       </section>
       {/* BUTTONS */}
